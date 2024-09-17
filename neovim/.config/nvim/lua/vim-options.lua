df99d745a216018fd433734ca63f3cf61fab5ac5
BryhanS <bryhansalazarpacsi@gmail.comm>
2024-09-17 08:09:30 -0500

    first commit

---
+++ neovim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,32 @@
+vim.g.mapleader = ' '
+vim.g.background = 'light'
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.swapfile = false
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set noswapfile ]]
+vim.cmd [[ set termguicolors ]]
+
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+
+--Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
